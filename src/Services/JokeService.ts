2753e3e2f028e8122507ca7a2bcb2087be282f39
danijel.vlah <danijel.vlah@RA-B707-MBP.local>
2023-10-20 15:55:00 +0200

    better error handling

--- src/Services/JokeService.ts
+++ src/Services/JokeService.ts
@@ -4,59 +4,75 @@ import * as nodeMailer from 'nodemailer'
 import { JokeType } from '../Types/JokeType'
 
 export class JokeService {
-	private static async getJoke(): Promise<JokeType>   {
-		const jokeResponse = await fetch('https://api.chucknorris.io/jokes/random')
-		if (!jokeResponse) {
-			throw new Error('Failed to fetch data')
+	private static async getJoke(res: Response): Promise<JokeType>   {
+		try {
+			const jokeResponse = await fetch('https://api.chucknorris.io/jokes/random')
+			if (!jokeResponse) {
+				throw new Error('Failed to fetch data')
+			}
+			const jokeJson = (await jokeResponse.json()) as JokeType
+			return jokeJson
+		} catch (error) {
+			console.error('Error logging in:', error)
+			res.status(500).json({ message: 'Internal server error' })
 		}
-		const jokeJson = (await jokeResponse.json()) as JokeType
-		return jokeJson
 	}
 
-	private static async extractEmailFromToken(req: Request): Promise<string | jwt.JwtPayload>  {
-		const authHeader = req.headers['authorization']
-		const token = authHeader && authHeader.split(' ')[1]
-		const email = jwt.decode(token)
-
-		return email
+	private static async extractEmailFromToken(req: Request, res: Response): Promise<string | jwt.JwtPayload>  {
+		try {
+			const authHeader = req.headers['authorization']
+			const token = authHeader && authHeader.split(' ')[1]
+			const email = jwt.decode(token)
+	
+			return email
+		} catch (error) {
+			console.error('Error logging in:', error)
+			res.status(500).json({ message: 'Internal server error' })
+		}
 	}
 
-	public static SendJokeToEmail = async (res: Response, req: Request) => {
-		const jokeToSend = await this.getJoke()
-		const email = await <string | jwt.JwtPayload>this.extractEmailFromToken(req)
+	public static async SendJokeToEmail(res: Response, req: Request): Promise<boolean>  {
+		try {
+			const jokeToSend = await this.getJoke(res)
+			const email = await <string | jwt.JwtPayload>this.extractEmailFromToken(req, res)
 
-		const transporter = nodeMailer.createTransport({
-			secure: false,
-			host: 'smtp.office365.com',
-			debug: true,
-			auth: {
-				user: process.env.CHUCKSEMAIL,
-				pass: process.env.CHUCKSPASS,
-			},
-		})
-
-		const jokeHtml = `<p>Hello there!</p>
-        <p>My name is Chakija Norris. I come from Chakovec and my chakija is ostra.
-          I'm sensing with my Chuck-E senses that you are having a bad day, so here is a funny joke for you!</p>
-          <b>${jokeToSend.value}</b>
-          <p> Best Regards!</p>
-          <p>Chuck</p>`
-
-		await transporter
-			.sendMail({
-				from: process.env.CHUCKSEMAIL,
-				to: typeof email === 'string' ? email : email.email,
-				subject: 'Chuck Norris Joke!',
-				text: 'Please confirm your email!!!',
-				html: jokeHtml
-			})
-			.then((success) => {
-				console.log('SUCCESS!', success)
-			})
-			.catch((error) => {
-				console.log('ERROR!', error)
+			const transporter = nodeMailer.createTransport({
+				secure: false,
+				host: 'smtp.office365.com',
+				debug: true,
+				auth: {
+					user: process.env.CHUCKSEMAIL,
+					pass: process.env.CHUCKSPASS,
+				},
 			})
 
-		res.send(jokeToSend)
+			const jokeHtml = `<p>Hello there!</p>
+					<p>My name is Chakija Norris. I come from Chakovec and my chakija is ostra.
+					I'm sensing with my Chuck-E senses that you are having a bad day, so here is a funny joke for you!</p>
+					<b>${jokeToSend.value}</b>
+					<p> Best Regards!</p>
+					<p>Chuck</p>`
+
+			await transporter
+				.sendMail({
+					from: process.env.CHUCKSEMAIL,
+					to: typeof email === 'string' ? email : email.email,
+					subject: 'Chuck Norris Joke!',
+					text: 'Please confirm your email!!!',
+					html: jokeHtml
+				})
+				.then((success) => {
+					console.log('SUCCESS!', success)
+				})
+				.catch((error) => {
+					console.log('ERROR!', error)
+				})
+
+			res.send(jokeToSend)
+			return true
+		} catch (error) {
+			console.error('Error logging in:', error)
+			res.status(500).json({ message: 'Internal server error' })
+		}
 	}
 }
--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -8,18 +8,29 @@ import { Response } from 'express'
 import { UserRefreshToken } from '../Entities/UserRefreshToken'
 
 export class UserService {
-	public static Login = async (
+	private static UserExists(user: UserRefreshToken | User): boolean{
+		if(!user){
+			return false
+		}
+
+		return true
+	} 
+
+	public static async Login (
 		{ email, password }: LoginInput,
 		res: Response
-	) => {
+	): Promise<boolean> {
 		try {
 			const userFromDb = await User.findOne({ where: { email } })
-			if (!userFromDb) return res.status(404).json({ message: 'User not found' })
+
+			this.UserExists(userFromDb) ? true : res.status(404).json({ message: 'User not found' })
 
 			const isPasswordValid = await bcrypt.compare(password, userFromDb.password)
-			if (!isPasswordValid)
-				return res.status(401).json({ message: 'Invalid password' })
-
+			if (!isPasswordValid){
+				res.status(401).json({ message: 'Invalid password' })
+				return false
+			}
+		
 			const accessToken = jwt.sign({ email }, process.env.JWT_SECRET_KEY_ACCESS, {
 				expiresIn: '20s',
 			})
@@ -34,23 +45,30 @@ export class UserService {
 				newRefreshToken.id = v4()
 				newRefreshToken.refreshToken = refreshToken
 				newRefreshToken.userId = userFromDb.id
+
 				await newRefreshToken.save()
 			}
 			res.status(201).json({ accessToken, refreshToken })
+
+			return true
 		} catch (error) {
 			console.error('Error logging in:', error)
 			res.status(500).json({ message: 'Internal server error' })
 		}
 	}
 
-	public static SignUp = async (
+	public static async SignUp (
 		{ firstName, lastName, email, password }: UserSignupInput,
 		res: Response
-	) => {
+	): Promise<boolean>{
 		try {
 			const userFromDb = await User.findOne({ where: { email } })
-			if (userFromDb)
-				return res.status(400).json({ message: 'User already exists' })
+			if (userFromDb){
+				res.status(400).json({ message: 'User already exists' })
+
+				return false
+			}
+				
 			const hashedPassword = await bcrypt.hash(password, 10)
 			const newUser = new User()
 
@@ -61,21 +79,25 @@ export class UserService {
 			newUser.password = hashedPassword
 			await newUser.save()
 			res.status(201).json({ message: 'User created successfully' })
+
+			return true
 		} catch (error) {
 			console.error('Error signing up:', error)
 			res.status(500).json({ message: 'Internal server error' })
 		}
 	}
 
-	public static Token = async (
+	public static async Token (
 		res: Response,
 		refreshTokenFromRequest: string
-	) => {
+	): Promise<boolean> {
 		try {
 			const userFromDb = await UserRefreshToken.findOne({
 				relations: ['user'],
 				where: { refreshToken: refreshTokenFromRequest },
 			})
+
+			this.UserExists(userFromDb) ? true : res.status(404).json({ message: 'User not found' })
 			jwt.verify(
 				userFromDb.refreshToken,
 				process.env.JWT_SECRET_KEY_REFRESH,
@@ -89,6 +111,9 @@ export class UserService {
 					res.json({ accessToken })
 				}
 			)
+			res.status(201).json({ message: 'User created successfully' })
+
+			return true
 		} catch (error) {
 			console.error('Verification error: ', error)
 			res.status(500).json({ message: 'Internal server error' })
